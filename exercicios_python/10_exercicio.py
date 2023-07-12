91a9efa751b3b1a02b2e5cbb448c2127d5f4be92
devrodrigobnu <devrodrigobnu@gmail.com>
2023-07-12 16:15:17 -0300

    update

--- exercicios_python/10_exercicio.py
+++ exercicios_python/10_exercicio.py
@@ -1,1 +1,13 @@
 # Escreva um programa que converta uma temperatura digitada em Celsius e converta para fahrenheit
+
+# Ler dados 
+celsius = float(input('Digite a temperatura em Celsius: '))
+fahrenheit = float(input('Digite a temperatura em Fahrenheit: '))
+
+# Converter dados
+f_to_c = (fahrenheit - 32) * 5/9
+c_to_f = (celsius * 9/5) + 32
+
+# Printar resultados
+print('A temperatura em celsius informada foi',celsius, 'e a sua conversão para fahrenheit é de:', c_to_f,'ºF')
+print('A temperatura em fahrenheit informada foi',fahrenheit, 'e a sua conversão para celsius é de:', f_to_c,'ºC')
--- exercicios_python/11_exercicio.py
+++ exercicios_python/11_exercicio.py
@@ -1,2 +1,12 @@
 # Escreva um programa que pergunte a quantidade de KM percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. 
 # Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0.15 por Km rodado.
+
+# Ler dados
+dias = int(input('Digite quantos dias o carro foi alugado: '))
+km = float(input('Digite quantos km foram rodados: '))
+
+# Converter dados
+preco_pagar = (60 * dias) + (km * 0.15)
+
+# Printar dados
+print('O total a pagar é de R$',preco_pagar)
