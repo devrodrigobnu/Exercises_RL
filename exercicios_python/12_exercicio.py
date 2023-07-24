c7366b9e8975d2c6c3e2becb24908930bf5df7cd
devrodrigobnu <devrodrigobnu@gmail.com>
2023-07-24 19:27:11 -0300

    update

--- exercicios_python/12_exercicio.py
+++ exercicios_python/12_exercicio.py
@@ -1,2 +1,9 @@
 # 12 - Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira
     # exemplo: Digite um número: 6.127. O número 6.127 tem a parte inteira 6.
+
+# Importar a função trunc (para quebrar o numero)  
+from math import trunc
+
+# Variável e print do resultado 
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
--- exercicios_python/13_exercicio.py
+++ exercicios_python/13_exercicio.py
@@ -1,1 +1,9 @@
 # 13 - Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa
+
+# Variáveis
+co = float(input('Digite o comprimento do cateto oposto: '))
+ca = float(input('Digite o comprimento do cateto adjacente: '))
+
+# Condição e print
+hipo = (co ** 2 + ca ** 2) ** (1/2)
+print('A hipotenusa vai medir {}'.format(hipo))
